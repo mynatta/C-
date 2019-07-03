2151bb7935cf5e1d4acac2affab72ac22b698c91
mynatta <52165302+mynatta@users.noreply.github.com>
2019-07-03 16:40:14 +0700

    Add files via upload

---
+++ Celcius.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std ;
+int main() {
+	int C ;
+	cout << "Enter Celcius Temperature : " ;
+	cin >> C ;
+	cout << C << " Celcius " << " = " << ( C*1.8 ) + 32  << " Fahrenheit " << endl ;
+	cout << "Now weather in Thailand is " << ((( C*1.8 ) + 32 > 70 )? " HOT! ":" COOL! " )<< endl ;
+}
