237dd37230f7b4f9594e78def376c58dc64e8a05
mynatta <52165302+mynatta@users.noreply.github.com>
2019-07-03 16:49:45 +0700

    Add files via upload

---
+++ Logical-Relational.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+
+	/*int Z , X ;
+	cout << " Enter first number (Z) : " ;
+	cin >> Z ;
+	cout << " Enter second number (X) : " ;
+	cin >> X ;
+	cout << endl ;
+	cout << " Testing Usage Operator " << endl ;
+	cout << " Z||X = " << (Z||X) << endl ;
+	return (0) ;*/
+
+	/*int A , B ;
+	cout << " Enter first number (A) : " ;
+	cin >> A ;
+	cout << " Enter second number (B) : " ;
+	cin >> B ;
+	cout << endl ;
+	cout << " Before A = " << A << endl ;
+	cout << " ++A = " << ++A << endl ;
+	cout << " After A = " << A << endl ;
+	cout << " Before A = " << A << endl ;
+    cout << " A++ = " << A++ << endl ;
+	cout << " After A = " << A << endl ;
+	cout << endl ;
+	cout << " Before B = " << B << endl ;
+	cout << " --B = " << --B << endl ;
+	cout << " After B = " << B << endl ;
+	cout << " Before B = " << B << endl ;
+	cout << " B-- = " << B-- << endl ;
+	cout << " After B = " << B << endl ; */
+	
+	/*int Number ;
+	cout << " Enter number : " ;
+	cin >> Number ;
+	cout << Number << " is " << ((Number % 2)==0 ? "Even" : "Odd") << "number" << endl ; */
+
+	int W ;
+	cout << " Enter Weight : " ;
+	cin >> W ;
+	cout << " Your Body is " << ((W >55 ) ? "Fat" : "Salender" ) << endl ;
+	 
+	
+	
+
+
+}
