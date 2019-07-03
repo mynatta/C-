3a9d61b819e79b315a65c6eae959a26946a5f902
mynatta <52165302+mynatta@users.noreply.github.com>
2019-07-03 16:51:35 +0700

    Add files via upload

---
+++ 7-2 G.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	/*string Name;
+	int Age;
+	cout << "Enter name : " ;
+	//cin >> Name;
+	getline(cin, Name) ;
+	cout << "Enter age : " ;
+	cin >> Age;
+	cout << endl ;
+	cout << "Hello, " << Name << "." << endl ;
+	cout << "You have " << Age << " years old." << endl ;
+	cout << "You are beginner programmer." << endl ;*/
+	float bath ;
+	cout << "Input Thai Baths : " ;
+	cin >> bath ;
+	cout << endl ;
+	cout << "****Exchange Rate**** " << endl ;
+	cout << bath << " Baths is " << bath*0.033 << " Dollars. " << endl ; 
+	cout << bath << " Baths is " << bath*0.029 << " Euros. " << endl ;
+	cout << bath << " Baths is " << bath*3.53 << " Yens. " << endl ;
+	cout << bath << " Baths is " << bath*0.022 << " China yuans. " << endl ;
+	cout << bath << " Baths is " << bath*761.52 << " Vietnam Dongs. " << endl ;
+
+	
+	
+	return (0);
+
+
+
+}		
